e85d46ccaee6957f3e6c21dfbfef9c5a58fd8590
powman <paulophpweb@gmail.com>
2023-06-03 20:43:34 -0300

    fdsa

--- _next/static/chunks/pages/index-c4aaf27c0d120827b897.js
+++ _next/static/chunks/pages/index-c4aaf27c0d120827b897.js
@@ -1406,50 +1406,50 @@
                                                                 )
                                                             )
                                                         ),
-                                                        C(
-                                                            "div",
-                                                            { className: "row" },
-                                                            C(
-                                                                "div",
-                                                                { className: "col-sm-12 col-md-12 col-lg-12" },
-                                                                C(
-                                                                    "div",
-                                                                    { className: "card" },
-                                                                    C(
-                                                                        "div",
-                                                                        { className: "card-body" },
-                                                                        C(
-                                                                            "div",
-                                                                            { className: "d-flex justify-content-between align-items-center buyback-card" },
-                                                                            C(
-                                                                                "button",
-                                                                                { style: { width: "100%" }, onClick: this.claim, disabled: this.state.loaded, type: "button", className: "waves-effect round btn btn-outline-primary btn-lg" },
-                                                                                C("span", { style: { color: "currentColor" }, className: "align-middle ms-25" }, this.state.claimbtnText),
-                                                                                "Connect Wallet" === this.state.claimbtnText
-                                                                                    ? C(
-                                                                                          p().Fragment,
-                                                                                          null,
-                                                                                          C(
-                                                                                              "svg",
-                                                                                              { xmlns: "http://www.w3.org/2000/svg", width: "14", height: "14", viewBox: "0 0 512 512", style: { marginLeft: "5px" } },
-                                                                                              C("path", {
-                                                                                                  fill: "#050505",
-                                                                                                  d:
-                                                                                                      "M95.5 104h320a87.73 87.73 0 0 1 11.18.71a66 66 0 0 0-77.51-55.56L86 94.08h-.3a66 66 0 0 0-41.07 26.13A87.57 87.57 0 0 1 95.5 104Zm320 24h-320a64.07 64.07 0 0 0-64 64v192a64.07 64.07 0 0 0 64 64h320a64.07 64.07 0 0 0 64-64V192a64.07 64.07 0 0 0-64-64ZM368 320a32 32 0 1 1 32-32a32 32 0 0 1-32 32Z",
-                                                                                              }),
-                                                                                              C("path", {
-                                                                                                  fill: "#050505",
-                                                                                                  d: "M32 259.5V160c0-21.67 12-58 53.65-65.87C121 87.5 156 87.5 156 87.5s23 16 4 16s-18.5 24.5 0 24.5s0 23.5 0 23.5L85.5 236Z",
-                                                                                              })
-                                                                                          )
-                                                                                      )
-                                                                                    : C(p().Fragment, null)
-                                                                            )
-                                                                        )
-                                                                    )
-                                                                )
-                                                            )
-                                                        ),
+                                                        // C(
+                                                        //     "div",
+                                                        //     { className: "row" },
+                                                        //     C(
+                                                        //         "div",
+                                                        //         { className: "col-sm-12 col-md-12 col-lg-12" },
+                                                        //         C(
+                                                        //             "div",
+                                                        //             { className: "card" },
+                                                        //             C(
+                                                        //                 "div",
+                                                        //                 { className: "card-body" },
+                                                        //                 C(
+                                                        //                     "div",
+                                                        //                     { className: "d-flex justify-content-between align-items-center buyback-card" },
+                                                        //                     C(
+                                                        //                         "button",
+                                                        //                         { style: { width: "100%" }, onClick: this.claim, disabled: this.state.loaded, type: "button", className: "waves-effect round btn btn-outline-primary btn-lg" },
+                                                        //                         C("span", { style: { color: "currentColor" }, className: "align-middle ms-25" }, this.state.claimbtnText),
+                                                        //                         "Connect Wallet" === this.state.claimbtnText
+                                                        //                             ? C(
+                                                        //                                   p().Fragment,
+                                                        //                                   null,
+                                                        //                                   C(
+                                                        //                                       "svg",
+                                                        //                                       { xmlns: "http://www.w3.org/2000/svg", width: "14", height: "14", viewBox: "0 0 512 512", style: { marginLeft: "5px" } },
+                                                        //                                       C("path", {
+                                                        //                                           fill: "#050505",
+                                                        //                                           d:
+                                                        //                                               "M95.5 104h320a87.73 87.73 0 0 1 11.18.71a66 66 0 0 0-77.51-55.56L86 94.08h-.3a66 66 0 0 0-41.07 26.13A87.57 87.57 0 0 1 95.5 104Zm320 24h-320a64.07 64.07 0 0 0-64 64v192a64.07 64.07 0 0 0 64 64h320a64.07 64.07 0 0 0 64-64V192a64.07 64.07 0 0 0-64-64ZM368 320a32 32 0 1 1 32-32a32 32 0 0 1-32 32Z",
+                                                        //                                       }),
+                                                        //                                       C("path", {
+                                                        //                                           fill: "#050505",
+                                                        //                                           d: "M32 259.5V160c0-21.67 12-58 53.65-65.87C121 87.5 156 87.5 156 87.5s23 16 4 16s-18.5 24.5 0 24.5s0 23.5 0 23.5L85.5 236Z",
+                                                        //                                       })
+                                                        //                                   )
+                                                        //                               )
+                                                        //                             : C(p().Fragment, null)
+                                                        //                     )
+                                                        //                 )
+                                                        //             )
+                                                        //         )
+                                                        //     )
+                                                        // ),
                                                         C(
                                                             "div",
                                                             { className: "row" },
